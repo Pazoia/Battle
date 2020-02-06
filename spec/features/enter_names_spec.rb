13b12a7f696c5172feb22d4c95f5ccad12f9e9ca
Paulo Azoia <pazoia.gomes@gmail.com>
2020-02-06 12:42:59 +0000

    Entering players complete

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'Enter name' do
+  scenario "submitting names" do
+    visit ('/')
+    fill_in :player_1_name, with: "Coding Course"
+    fill_in :player_2_name, with: "Paulo"
+    click_button "Submit"
+    expect(page).to have_content "Coding Course vs. Paulo"
+  end
+end
