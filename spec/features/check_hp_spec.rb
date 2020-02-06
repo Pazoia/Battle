2bf5146e9818a51d51ba01645e232333402165c9
Paulo Azoia <pazoia.gomes@gmail.com>
2020-02-06 14:51:14 +0000

    The Helpers --> Added 'web_helpers.rb' to features

--- spec/features/check_hp_spec.rb
+++ spec/features/check_hp_spec.rb
@@ -1,9 +1,6 @@
 feature 'check hp' do 
-  scenario "see player hp" do 
-    visit ('/')
-    fill_in :player_1_name, with: "Joe"
-    fill_in :player_2_name, with: "Paulo"
-    click_button "Submit"
+  scenario "see player hp" do
+    sign_in_and_play
     expect(page).to have_content "Paulo HP:100"
   end 
 end 
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,10 +1,6 @@
 feature 'Enter name' do
   scenario "submitting names" do
-    visit ('/')
-    fill_in :player_1_name, with: "Coding Course"
-    fill_in :player_2_name, with: "Paulo"
-    click_button "Submit"
-    expect(page).to have_content "Coding Course vs. Paulo"
-    
+    sign_in_and_play
+    expect(page).to have_content "Joe vs. Paulo"    
   end
 end
